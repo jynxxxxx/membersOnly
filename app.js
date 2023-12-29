eefffa6774309b2946c39969455f808d3c2014f2
jynxxxxx <137763251+jynxxxxx@users.noreply.github.com>
2023-12-29 17:48:13 +0900

    make adjustments for vercel deploy

--- app.js
+++ app.js
@@ -30,5 +30,12 @@ app.use((req, res, next) => {
 
 app.use('/', indexRouter);
 
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
 
-app.listen(3000, () => console.log("app listening on port 3000!"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`app listening on port ${port}!`));
+
+
+module.exports = app;
